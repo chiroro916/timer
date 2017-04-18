845dfa5c01260d9032c74b45faf29e8e9cf25247
Chihiro <Chihiro@M.local>
2017-04-18 22:30:28 +0900

    timerアプリ

--- timer/ViewController.swift
+++ timer/ViewController.swift
@@ -10,16 +10,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    // タイマー用の時間のための変数
+    var timer: Timer!
+    var timer_sec: Float = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+    
+        // タイマーの作成、始動
+        Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // selector: #selector(updatetimer) で指定された関数
+    // timeInterval: 0.1, repeats: true で指定された通り、0.1秒毎に呼び出され続ける
+        func updateTimer(timer: Timer) {
+            self.timer_sec += 0.1
+            self.timerLabel.text = String(format: "%.1f", timer_sec)
+    }
 
-
+    
+    // 再生ボタン IBAction
+    @IBAction func startTimer(_ sender: Any) {
+    
+        // 動作中のタイマーを1つに保つために、 timer が存在しない場合だけ、タイマーを生成して動作させる
+        if self.timer == nil {
+        self.timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        }
+    }
+    
+    // 一時停止ボタン IBAction
+    @IBAction func pauseTimer(_ sender: Any) {
+        if self.timer != nil {
+            self.timer.invalidate()   // 現在のタイマーを破棄する
+            self.timer = nil          // startTimer() の timer == nil で判断するために、 timer = nil としておく
+        }
+    }
+    
+    // リセットボタン IBAction
+    @IBAction func resetTimer(_ sender: Any) {
+    
+        
+        self.timer_sec = 0
+        self.timerLabel.text = String(format: "%.1f", self.timer_sec)
+        
+        if self.timer != nil {
+            self.timer.invalidate()   // 現在のタイマーを破棄する
+            self.timer = nil          // startTimer() の timer == nil で判断するために、 timer = nil としておく
+        }
+    }
 }
-
